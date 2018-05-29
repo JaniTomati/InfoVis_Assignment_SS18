02509d582e3d65d42b1aa666766891e0eb7def9e
Jojooo0o <jonasdorsch@web.de>
2018-05-29 12:09:42 +0200

    assignment 3 finished

--- src/infovis/paracoords/View.java
+++ src/infovis/paracoords/View.java
@@ -4,9 +4,12 @@ import infovis.scatterplot.Data;
 import infovis.scatterplot.Model;
 
 import java.awt.*;
+import java.awt.geom.Line2D;
+import java.awt.geom.Point2D;
 import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 
+import javax.sound.sampled.Line;
 import javax.swing.JPanel;
 
 public class View extends JPanel {
@@ -29,10 +32,12 @@ public class View extends JPanel {
 		ArrayList<String> labels = model.getLabels();
 
 		ArrayList<Data> data = model.getList();
-		ArrayList<Point> data_point_array = new ArrayList<Point>();
+		ArrayList<Point2D> data_point_array = new ArrayList<Point2D>();
+		ArrayList<Line2D> lines = new ArrayList<Line2D>();
 
 		for (int i = 0; i < amount_labels; ++i) {
 			g2D.drawLine(i * LINEDISTANCE + 20, YLINECOORD, i * LINEDISTANCE + 20, YLINECOORD + LINELENGTH);
+			g2D.setColor(Color.BLACK);
 			g2D.setFont(new Font("Arial", Font.BOLD, 10));
 			g2D.drawString(labels.get(i), i * LINEDISTANCE + 20, YLINECOORD - 10);
 
@@ -54,25 +59,33 @@ public class View extends JPanel {
 				}
 				count++;
 
-				Point data_point = new Point(i * LINEDISTANCE + 20,
+				Point2D data_point = new Point2D.Double(i * LINEDISTANCE + 20,
 						(int) (((d.getValue(i) - min_height) * LINELENGTH) / (max_height - min_height) + YLINECOORD));
 
 				data_point_array.add(data_point);
 				// draw data
 				//g2D.fillOval(data_point.x, data_point.y, 7, 7);
-				g2D.drawOval(data_point.x - 3, data_point.y - 3, 6, 6);
+				g2D.setColor(d.getColor());
+				g2D.drawOval((int) ((Point2D.Double) data_point).x - 3, (int) ((Point2D.Double) data_point).y - 3, 6, 6);
 				if(markerRectangle.contains(data_point)) {
-					d.setColor(Color.RED);
+					//d.setColor(Color.RED);
 				}
 			}
 		}
 
 		for(int i = 0; i < data_point_array.size()-1; ++i) {
 			if(i >= data.size()) {
+				lines.add(new Line2D.Double(data_point_array.get(i), data_point_array.get(i-data.size())));
 				g2D.setColor(data.get(i % data.size()).getColor());
-				g2D.drawLine(data_point_array.get(i).x, data_point_array.get(i).y,
-						data_point_array.get(i - data.size()).x, data_point_array.get(i - data.size()).y);
+				g2D.drawLine((int) data_point_array.get(i).getX(),(int) data_point_array.get(i).getY(),
+						(int) data_point_array.get(i - data.size()).getX(), (int) data_point_array.get(i - data.size()).getY());
 				g2D.setColor(Color.BLACK);
+			}
+		}
+		for(int i = 0; i < lines.size(); ++i) {
+			if(markerRectangle.intersectsLine(lines.get(i))){
+				data.get(i % data.size()).setColor(Color.RED);
+				this.repaint();
 			}
 		}
 	}
